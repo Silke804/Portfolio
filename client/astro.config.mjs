4f9720b1ef00217e75fc31ec43e1481ca7fcf837
Silke Van den Brempt <silke.van.den.brempt@student.howest.be>
2025-11-10 16:29:25 +0100

    tes

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://silke804.github.io/',
+    site: 'https://silke804.github.io/Portfolio/',
     base: '/Portfolio/',
 });
